2e65ed40ca3aa25ea039d67bf2f235fdadc104ce
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2023-06-05 00:48:17 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 284 ms, Memory: 34812 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,8 +1,20 @@
-from collections import Counter
+from string import ascii_lowercase
+from collections import defaultdict
 
 
 s = input()
 q = int(input())
+
+# prefix_sum
+prefix_sum = defaultdict(list)
+for alpha in ascii_lowercase:
+    prefix_sum[alpha].append(0)
+    alpha_cnt = 0
+    for i in range(len(s)):
+        if s[i] == alpha:
+            alpha_cnt += 1
+        prefix_sum[alpha].append(alpha_cnt)
+					
 for _ in range(q):
     alpha, l, r = input().split()
-    print(Counter(s[int(l):int(r)+1])[alpha])
+    print(prefix_sum[alpha][int(r)+1] - prefix_sum[alpha][int(l)])
