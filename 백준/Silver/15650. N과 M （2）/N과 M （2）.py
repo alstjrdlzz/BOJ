9b9b74a2d749642b2ce5fcde0b5201d225a15346
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2023-06-30 23:31:12 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,12 @@
+def dfs(start):
+    if len(s) == m:
+        print(" ".join(map(str, s)))
+        return
+    for i in range(start, n+1):
+        s.append(i)
+        dfs(i+1)
+        s.pop()
+            
+n, m = map(int,input().split())
+s = []
+dfs(1)
