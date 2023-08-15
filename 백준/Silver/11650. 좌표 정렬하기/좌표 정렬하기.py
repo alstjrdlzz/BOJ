7e06c79b39beb557e43d950b75d1f061eaad855a
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2023-08-15 21:10:01 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 372 ms, Memory: 59988 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,14 @@
+import sys
+
+
+input = sys.stdin.readline
+n = int(input())
+
+points = []
+for _ in range(n):
+    points.append(list(map(int, input().split())))
+
+sorted_points = sorted(points, key=lambda x: (x[0], x[1]))
+
+for i in range(n):
+    print(sorted_points[i][0], sorted_points[i][1])
