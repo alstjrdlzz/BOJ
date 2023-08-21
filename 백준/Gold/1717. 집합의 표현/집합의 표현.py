c71175792cb8a4b81710e7b97f235da9673e92d1
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2023-08-21 22:08:45 +0900

    [Gold V] Title: 집합의 표현, Time: 296 ms, Memory: 76720 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,31 +1,33 @@
 import sys
-sys.setrecursionlimit(1000000) # 재귀 깊이 제한 늘리기
+sys.setrecursionlimit(1000000)
+
+
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-parent = [i for i in range(n + 1)] # 자기 자신을 부모로 갖는 n + 1개 집합
 
-# 찾기 연산(같은 집합에 속하는지 확인하기 위한 함수)
-def find_parent(x):
+def find(x):
     if parent[x] != x:
-        parent[x] = find_parent(parent[x])
+        parent[x] = find(parent[x])
     return parent[x]
 
-# 합집합 연산(두 집합을 합치기 위한 함수)
-def union_parent(a, b):
-    a = find_parent(a)
-    b = find_parent(b)
-    if a < b: # 값이 더 작은 쪽을 부모로
-        parent[b] = a
+    
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    
+    if x < y:
+        parent[y] = x
     else:
-        parent[a] = b
-        
+        parent[x] = y
+
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
 for _ in range(m):
-    opr, a, b = map(int, input().split())
-    if opr == 0:
-        union_parent(a, b)
-    else:
-        if find_parent(a) == find_parent(b):
+    operator, a, b = map(int, input().split())
+    if operator == 0:
+        union(a, b)
+    elif operator == 1:
+        if find(a) == find(b):
             print("YES")
         else:
             print("NO")
