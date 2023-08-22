4c68db3ff1ff7833540c25f5ddfa09468722ff11
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2023-08-22 23:19:37 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 528 ms, Memory: 58420 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**9)
+
+
+input = sys.stdin.readline
+
+
+def bfs(graph, v, visited):
+    global cnt
+    
+    visited[v] = cnt
+
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if visited[i] == 0:
+                cnt += 1
+                visited[i] = cnt
+                queue.append(i)
+          
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+cnt = 1
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, n + 1):
+    graph[i].sort()
+    
+queue = deque([r])
+visited[r] = cnt
+bfs(graph, r, visited)
+for i in range(1, n + 1):
+    print(visited[i])
