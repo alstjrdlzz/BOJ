deb09d412e8c4d5fa5c8dc23b06dc49a4b927d70
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2023-09-04 21:11:55 +0900

    [Silver II] Title: 종이의 개수, Time: 4668 ms, Memory: 70524 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,21 +1,27 @@
 import sys
+from collections import defaultdict
 input = sys.stdin.readline
 
 
-result = {-1: 0, 0: 0, 1: 0}
+result = defaultdict(int)
+
+
+def divide(x, y, size, k):
+    size = size // k
+    for a in range(k):
+        for b in range(k):
+            nona_tree(x + a * size, y + b * size, size)
 
 def nona_tree(x, y, size):
     start = board[x][y]
     for i in range(x, x + size):
         for j in range(y, y + size):
             if start != board[i][j]:
-                size = size // 3
-                for a in range(3):
-                    for b in range(3):
-                        nona_tree(x + a * size, y + b * size, size)
+                divide(x, y, size, 3)
                 return
+            
     result[start] += 1
-    
+
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
 
