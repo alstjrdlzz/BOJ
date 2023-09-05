15d9170cb60a5ffa4c2d942e89a55e7cd146aafc
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2023-09-05 21:31:34 +0900

    [Silver II] Title: 이항 계수 2, Time: 256 ms, Memory: 87456 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+
+def solve(n, k):
+    for i in range(n + 1):
+        dp[i][0] = 1
+    for i in range(k + 1):
+        dp[i][i] = 1
+        
+    for i in range(1, n + 1):
+        for j in range(1, k + 1):
+            dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1]
+
+    return dp[n][k] % 10007
+
+
+n, k = map(int, input().split())
+
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+
+print(solve(n, k))
