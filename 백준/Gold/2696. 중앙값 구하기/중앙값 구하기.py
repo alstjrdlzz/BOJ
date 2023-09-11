1c670f775eb39e01f8cdfbc6151f92415694da04
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2023-09-11 17:47:40 +0900

    [Gold II] Title: 중앙값 구하기, Time: 56 ms, Memory: 34348 KB -BaekjoonHub

---
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -0,0 +1,53 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+
+def solution(S):
+    small = []
+    big = []
+    median = S[0]
+    medians = [median]
+    for i in range(1, len(S)):
+        x = S[i]
+        if x > median:
+            heapq.heappush(big, x)
+        else:
+            heapq.heappush(small, (-x, x))
+        
+        if i % 2 == 0:
+            if len(small) < len(big):
+                heapq.heappush(small, (-median, median))
+                median = heapq.heappop(big)
+            elif len(small) > len(big):
+                heapq.heappush(big, median)
+                median = heapq.heappop(small)[1]
+            medians.append(median)
+    return medians
+         
+    
+t = int(input())
+for _ in range(t):
+    m = int(input())
+    
+    if m % 10 == 0:
+        num_lines = m // 10
+    else:
+        num_lines = m // 10 + 1
+        
+    S = []
+    for _ in range(num_lines):
+        S.extend(list(map(int, input().split())))
+    
+    medians = solution(S)
+    
+    print(len(medians))
+    
+    cnt = 0
+    for median in medians:
+        print(median, end=" ")
+        cnt += 1
+        if cnt == 10:
+            print()
+            cnt = 0
+    print()
