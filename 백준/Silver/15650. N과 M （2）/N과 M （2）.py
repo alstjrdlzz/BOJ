b4406c021a04b49ea5aa6b73e445472b408b3d51
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-03-31 13:20:56 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,12 +1,17 @@
-def dfs(start):
-    if len(s) == m:
-        print(" ".join(map(str, s)))
+import sys
+input = sys.stdin.readline
+
+def dfs(n, lst):
+    if n > N:
+        if len(lst) == M:
+            ans.append(lst)
         return
-    for i in range(start, n+1):
-        s.append(i)
-        dfs(i+1)
-        s.pop()
-            
-n, m = map(int,input().split())
-s = []
-dfs(1)
+    dfs(n + 1, lst + [n])
+    dfs(n + 1, lst)
+    
+    
+N, M = map(int, input().split())
+ans = []
+dfs(1, [])
+for lst in ans:
+    print(*lst)
