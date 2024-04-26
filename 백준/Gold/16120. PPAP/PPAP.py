eaaab4cad5c3b7cb5a1e748a9e6ba3a8ae6bc9bf
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-04-26 17:20:24 +0900

    [Gold IV] Title: PPAP, Time: 324 ms, Memory: 38168 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.py
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -1,13 +1,15 @@
-w = input()
-stack = []
-ppap = ["P", "P", "A", "P"]
-for i in range(len(w)):
-    stack.append(w[i])
-    if stack[-4:] == ppap:
-        for _ in range(4):
-            stack.pop()
-        stack.append("P")
-if stack == ppap or stack == ["P"]:
-    print("PPAP")
-else:
-    print("NP")
+def sol(string):
+    stack = []
+    for ch in string:
+        stack.append(ch)
+        if stack[-4:] == ["P", "P", "A", "P"]:
+            for _ in range(4):
+                stack.pop()
+            stack.append("P")
+    if stack == ["P", "P", "A", "P"] or stack == ["P"]:
+        return "PPAP"
+    return "NP"
+
+
+string = input()
+print(sol(string))
