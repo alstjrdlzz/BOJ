0fb00ef710816761591b058306ce4a445607f5a4
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-05-03 17:01:59 +0900

    [Gold I] Title: Gaaaaaaaaaarden, Time: 1896 ms, Memory: 123420 KB -BaekjoonHub

---
+++ 백준/Gold/18809. Gaaaaaaaaaarden/Gaaaaaaaaaarden.py
@@ -0,0 +1,74 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def bfs(tlst):
+    # [0] 큐 생성 및 v 등 필요한 변수생성
+    q = deque([])
+    v = [[0] * (M + 2) for _ in range(N + 2)]
+    
+    # [1] 큐에 초기데이터들 삽입(v표시)
+    for i in range(TC):
+        if tlst[i] == 0: continue
+        ti, tj = lst[i]
+        q.append((ti, tj))
+        v[ti][tj] = tlst[i]
+        
+    cnt = 0
+    while q:
+        ci, cj = q.popleft()
+        if v[ci][cj] == 25000: continue
+            
+        # 네방향, 범위내(x), (미방문), 호수나 꽃이 아니면
+        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            ni, nj = ci + di, cj + dj
+            if arr[ni][nj] == 0 or v[ni][nj] == 25000:
+                continue
+                
+            if v[ni][nj] == 0: # 처음 방문
+                if v[ci][cj] < 0: # R: 1 감소
+                    v[ni][nj] = v[ci][cj] - 1
+                    q.append((ni, nj))
+                else: # G: 1 증가
+                    v[ni][nj] = v[ci][cj] + 1
+                    q.append((ni, nj))  
+            else:                # 이미 기록 => 꽃 체크
+                if v[ci][cj] < 0:
+                    if v[ni][nj] + v[ci][cj] - 1 == 0:
+                        cnt += 1
+                        v[ni][nj] = 25000
+                else:
+                    if v[ni][nj] + v[ci][cj] + 1 == 0:
+                        cnt += 1
+                        v[ni][nj] = 25000
+    return cnt
+
+
+
+def dfs(n, rcnt, gcnt, tlst):
+    global ans
+    if n == TC: # 모든 땅을 결정했으면 종료
+        if rcnt == RC and gcnt == GC:
+            ans = max(ans, bfs(tlst))
+        return
+    
+    dfs(n + 1, rcnt + 1, gcnt, tlst + [-1]) # 빨간 씨앗
+    dfs(n + 1, rcnt, gcnt + 1, tlst + [1])  # 초록 씨앗
+    dfs(n + 1, rcnt, gcnt, tlst + [0])      # 안 뿌리는 경우
+
+    
+N, M, RC, GC = map(int, input().split())
+# 호수로 둘러싸기
+arr = [[0] * (M + 2)] + [[0] + list(map(int, input().split())) + [0] for _ in range(N)] + [[0] * (M + 2)]
+
+lst = []
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        if arr[i][j] == 2: # 배양이 가능한 땅이면 좌표 저장
+            lst.append((i, j))
+TC = len(lst)
+
+# [1] 가능한 모든 장소에 배양액 뿌리는 방법 순회
+ans = 0
+dfs(0, 0, 0, [])
+print(ans)
