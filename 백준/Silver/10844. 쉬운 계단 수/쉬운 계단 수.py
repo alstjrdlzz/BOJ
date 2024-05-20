0cd27cf97e3ce7d538e6165ce21dc36e6d3aa2a0
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-05-20 16:52:32 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,22 +1,13 @@
 import sys
 input = sys.stdin.readline
 
+N = int(input())
+dp = [[0] * (12) for _ in range(101)]
+dp[1][2:11] = [1] * 9
 
-def solution():
-    d = [[0] * 10 for _ in range(n + 1)]
-    
-    for j in range(1, 10):
-        d[1][j] = 1
+for i in range(2, N + 1):
+    for j in range(1, 11):
+        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
         
-    for i in range(2, n + 1):
-        for j in range(10):
-            if j == 0:
-                d[i][j] = d[i - 1][j + 1]
-            elif j == 9:
-                d[i][j] = d[i - 1][j - 1]
-            else:
-                d[i][j] = d[i - 1][j - 1] + d[i - 1][j + 1] 
-    return sum(d[n]) % 1000000000
-
-n = int(input())
-print(solution())
+ans = sum(dp[N])
+print(ans % 10**9)
