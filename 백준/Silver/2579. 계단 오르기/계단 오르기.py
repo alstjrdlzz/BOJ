13ae58e79c1241a5cd1ae4f4f8123800412c356c
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-06-03 14:44:56 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,23 +1,17 @@
 import sys
 input = sys.stdin.readline
 
+N = int(input())
+lst = [0] * 301
+for i in range(N):
+    lst[i + 1] = int(input())
 
-def solution():
-    d = [0] * 301
-    d[1] = stairs[1]
-    d[2] = stairs[1] + stairs[2]
-    d[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
-    for i in range(4, n + 1):
-        case1 = d[i - 3] + stairs[i - 1] + stairs[i]
-        case2 = d[i - 2] + stairs[i]
-        d[i] = max(case1, case2)
-        
-    return d[n]
+dp = [0] * 301
 
-    
-n = int(input())
-stairs = [0] * 301
-for i in range(1, n + 1):
-    stairs[i] = int(input())
+dp[1] = lst[1]
+dp[2] = lst[1] + lst[2]
+dp[3] = max(lst[1], lst[2]) + lst[3]
+for i in range(4, 301):
+    dp[i] = max(dp[i - 3] + lst[i - 1], dp[i - 2]) + lst[i]
 
-print(solution())
+print(dp[N])
