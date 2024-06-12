e27104655d45757ae1eb219aa260af51a0c55e21
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-06-12 20:30:40 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,47 +1,33 @@
 import sys
+input = sys.stdin.readline
 from collections import deque
 
-
-input = sys.stdin.readline
-
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-
-def bfs(a, b):
-    queue = deque([(a, b)])
-    graph[a][b] = 0
+def bfs(si, sj):
+    q = deque([(si, sj)])    # 초기 데이터 삽입
+    arr[si][sj] = 0          # 초기 데이터 방문 표시
     
     cnt = 1
-    while queue:
-        x, y = queue.popleft()
-        
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-                
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
+    while q:
+        ci, cj = q.popleft()
+        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            ni, nj = ci + di, cj + dj
+            if arr[ni][nj] == 1:      # 방문하지 않은 경우
+                q.append((ni, nj))    # 큐에 삽입
+                arr[ni][nj] = 0       # 방문 표시
                 cnt += 1
     return cnt
 
-    
-n = int(input())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().strip())))
-    
-answer = []
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            answer.append(bfs(i, j))
 
-print(len(answer))
+N = int(input())
+arr = [[0] * (N + 2)] + [[0] + list(map(int, input().strip())) + [0] for _ in range(N)] + [[0] * (N + 2)]
 
-answer.sort()
-for cnt in answer:
+lst = []
+for i in range(N + 2):
+    for j in range(N + 2):
+        if arr[i][j] == 1:
+            lst.append(bfs(i, j))
+            
+lst.sort()        # 오름차순 정렬
+print(len(lst))
+for cnt in lst:
     print(cnt)
