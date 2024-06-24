a4ff6a2dc721774403c8f31fe6cc0822fec46e45
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-06-24 15:26:25 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 92 ms, Memory: 34072 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def bfs(sj, si, ej, ei):
+    q = deque([])
+    v = [0] * N
+    
+    q.append((sj, si))
+    
+    while q:
+        cj, ci = q.popleft()
+        if abs(cj - ej) + abs(ci - ei) <= 1000 :
+            return "happy"
+        
+        for i in range(N):
+            if v[i] == 0:
+                nj, ni = lst[i]
+                if abs(cj - nj) + abs(ci - ni) <= 1000:
+                    q.append((nj, ni))
+                    v[i] = 1
+    return "sad"
+
+
+TC = int(input())
+for _ in range(TC):
+    N = int(input())
+    sj, si = map(int, input().split())
+    lst = []
+    for _ in range(N):
+        tj, ti = map(int, input().split())
+        lst.append((tj, ti))
+    ej, ei = map(int, input().split())
+    ans = bfs(sj, si, ej, ei)
+    print(ans)
