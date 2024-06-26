aba41d2dd10f8eb565165c7a01bd0ba4979b0b6f
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-06-26 16:59:10 +0900

    [Bronze III] Title: ZOAC 4, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
@@ -1,11 +1,10 @@
 import sys
 input = sys.stdin.readline
+import math
 
 H, W, N, M = map(int, input().split())
 
-cnt = 0
-for i in range(0, H, N + 1):
-    for j in range(0, W, M + 1):
-        cnt += 1
-            
-print(cnt)
+r = math.ceil(H/(N + 1))
+c = math.ceil(W/(M + 1))
+       
+print(r * c)
