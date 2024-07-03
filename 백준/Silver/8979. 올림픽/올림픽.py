5150ac0daf583570b5b1d9516ecbae15fa6fff0c
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-07-03 10:58:48 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -2,26 +2,16 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-dic = {}
-for _ in range(N):
-    nid, g, s, b = map(int, input().split())
-    dic[nid] = (g, s, b)
-    
-kg, ks, kb = dic[K]    
-cnt = 0
-for cid in range(1, N + 1):
-    if cid == K:
-        continue
-        
-    cg, cs, cb = dic[cid]
-    if cg > kg:
-        cnt += 1
-        break
-    elif (cg == kg) and (cs > ks):
-        cnt += 1
-        break
-    elif (cs == ks) and (cb > kb):
-        cnt += 1
-        break
+medal_list = [list(map(int, input().split())) for _ in range(N)]
+medal_list.sort()
 
-print(cnt + 1)
+k, kg, ks, kb = medal_list[K-1]
+cnt = 1
+
+for idx, g, s, b in medal_list :
+  if idx == K :
+    continue
+  if g > kg or g == kg and s > ks or g == kg and s == ks and b > kb :
+    cnt += 1
+
+print(cnt)
