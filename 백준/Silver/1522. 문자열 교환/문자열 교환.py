6cff56dab4f2d2bfc2cd00bf83ec0b9b32329eb2
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-07-24 10:38:06 +0900

    [Silver I] Title: 문자열 교환, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.py
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.py
@@ -2,18 +2,13 @@ import sys
 input = sys.stdin.readline
 
 st = input().rstrip()
-
 n = len(st)
+window_size = st.count("a")
+st += st[:n]
 
 ans = 1001
 for s in range(n):
-    e = s + st.count("a")
-    
-    if e > n:
-        window = st[s:n] + st[:e - n]
-    else:
-        window = st[s:e]
-        
+    window = st[s:s+window_size]  
     ans = min(ans, window.count("b"))
     
 print(ans)
