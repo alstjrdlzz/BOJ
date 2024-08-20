46585aaa98f18a1907c99d1485381cbc1926bdbe
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-08-20 15:10:51 +0900

    [Silver III] Title: 타노스, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,28 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def sol(st, ch, trg):
-    cnt = 0
-    i = 0
-    while cnt < trg:
-        if st[i] == ch:
-            st = st[:i] + st[i + 1:]
-            cnt += 1
-        i += 1
-    
-    return st
-
 
 S = input().strip()
 
+trg0 = S.count("0") // 2
+trg1 = S.count("1") // 2
+
+ans = ""
 cnt0, cnt1 = 0, 0
 for ch in S:
     if ch == "0":
-        cnt0 += 1
+        if cnt0 < trg0:
+            ans += ch
+            cnt0 += 1
     else:
-        cnt1 += 1
-
-S = sol(S, "1", cnt1//2)
-S = sol(S[::-1], "0", cnt0//2)
-
-print(S[::-1])
+        if cnt1 < trg1:
+            cnt1 += 1
+        else:
+            ans += ch
+print(ans)
