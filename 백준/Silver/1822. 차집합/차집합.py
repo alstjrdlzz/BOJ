e399a00863315201e51c0ba08eeb1bb00e3ef659
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2024-11-27 19:16:42 +0900

    [Silver IV] Title: 차집합, Time: 976 ms, Memory: 149960 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+nA, nB = map(int, input().split())
+
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+
+print(len(A - B))
+print(*sorted(list(A - B)))
