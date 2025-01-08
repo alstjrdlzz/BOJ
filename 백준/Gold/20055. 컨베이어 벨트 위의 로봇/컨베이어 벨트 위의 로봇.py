4679647965bd232bec093dd4c867b4719d8ddd87
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2025-01-08 20:50:08 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 2180 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -5,23 +5,29 @@ N, K = map(int, input().split())
 belt = list(map(int, input().split()))
 robot = [0] * N
 
+cnt = 0
 ans = 0
-while 1: # 내구도 체크
+while 1:
     ans += 1
     # [1]
-    belt = [belt[-1]] + belt[:-1]      
-    robot = [0] + robot[:-1]
+    belt.insert(0, belt.pop())     
+    robot.pop()
+    robot.insert(0, 0)
     robot[N - 1] = 0
-    # [2] 로봇 1칸 이동
+    # [2]
     for i in range(N - 2, 0, -1):
         if robot[i] == 1 and robot[i + 1] == 0 and belt[i + 1] >= 1:
             robot[i], robot[i + 1] = 0, 1 # 이동
             belt[i + 1] -= 1    
-            
+            if belt[i + 1] == 0:
+                cnt += 1
+    # [3]       
     if belt[0] >= 1:
-        robot[0] = 1 # 이동
-        belt[0] -= 1 # 내구도 감소
-        
-    if belt.count(0) >= K:
+        robot[0] = 1
+        belt[0] -= 1
+        if belt[0] == 0:
+            cnt += 1
+    # [4]
+    if cnt >= K:
         break        
 print(ans)
