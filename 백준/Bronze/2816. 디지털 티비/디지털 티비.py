3d0f2bdc02d7a27997123fd50f25f72f9f456d58
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2025-01-10 11:25:43 +0900

    [Bronze I] Title: 디지털 티비, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2816. 디지털 티비/디지털 티비.py
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.py
@@ -5,10 +5,10 @@ N = int(input())
 lst = [input().rstrip() for _  in range(N)]
 
 ans = ""
-KBS1_idx = lst.index("KBS1")
-lst.insert(0, lst.pop(KBS1_idx))
-ans += ("1" * KBS1_idx + "4" * KBS1_idx)
+kbs1_idx = lst.index("KBS1")
+lst.insert(0, lst.pop(kbs1_idx))
+ans += ("1" * kbs1_idx + "4" * kbs1_idx)
 
-KBS2_idx = lst.index("KBS2")
-ans += ("1" * KBS2_idx + "4" * (KBS2_idx - 1))
+kbs2_idx = lst.index("KBS2")
+ans += ("1" * kbs2_idx + "4" * (kbs2_idx - 1))
 print(ans)
