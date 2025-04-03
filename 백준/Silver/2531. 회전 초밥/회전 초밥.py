501f6bc4777375016a19faaddee1790f55303cf4
서민석(Minseok Seo) <114855093+alstjrdlzz@users.noreply.github.com>
2025-04-03 16:31:47 +0900

    [Silver I] Title: 회전 초밥, Time: 84 ms, Memory: 35216 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,17 +1,25 @@
 import sys
 input = sys.stdin.readline
+from collections import defaultdict
 
 N, D, K, C = map(int, input().split())
 lst = [int(input()) for _ in range(N)]
 lst += lst[:-1]
 
+window = defaultdict(int)
+for i in range(K):
+    window[lst[i]] += 1
+window[C] += 1
+    
+ans = len(window)
+
 start, end = 0, K
-window = lst[start : end]
-ans = len(set(window + [C]))
 while start < N:
-    window.pop(0)
-    window += [lst[end]]
-    ans = max(ans, len(set(window + [C])))
+    window[lst[start]] -= 1
+    if window[lst[start]] == 0:
+        window.pop(lst[start])
+    window[lst[end]] += 1
+    ans = max(ans, len(window))
     start += 1
     end += 1
 print(ans)
